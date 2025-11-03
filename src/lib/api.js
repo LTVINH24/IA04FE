7c1307e7f3bb3ea90113c664bf4467ffa35c2162
Le Thanh Vinh <thanhvinh2400@gmail.com>
2025-11-03 17:52:02 +0700

    add .env

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 import tokenManager from './tokenManager';
-
-const API_BASE_URL = 'https://ia04be.onrender.com';
-
+const  API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
   headers: {
